8bad28901a907caa993d47de3178c4fe657bba33
phenix-factory <trash@henix.be>
2013-07-24 17:22:34 +0200

    Fichier de lang du site.

---
+++ squelettes/lang/local_fr.php
@@ -0,0 +1,33 @@
+<?php 
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    // A
+    // B
+    // C
+    // D
+    // E
+    // F
+    // G
+    // H
+    // I
+    // J
+    // L
+    // N
+    // M
+    // O
+    // P
+    // Q
+    // R
+    'recherche' => 'recherche'
+    // S
+    // T
+    // U
+    // V
+    // W
+    // X
+    // Y
+    // Z
+);
+
+?>
